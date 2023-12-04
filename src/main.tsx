61d39cf1cf7190a47aba16cfa025902788c6f698
Yehuda Koth <ykot@gmail.com>
2023-12-04 12:22:44 +0200

    apolloProvider done

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import { CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo/apolloApi.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <CssBaseline />
-      <App />
-    </Provider>
-  </BrowserRouter>
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <Provider store={store}>
+                <CssBaseline />
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </ApolloProvider>
 );
