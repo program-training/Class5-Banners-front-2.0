5cf81efcc67014a79e769e4e5e684350f0cf3027
itzik <itsikzigi@gmail.com>
2023-12-04 09:57:18 +0200

    Initial repo

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { checker } from "vite-plugin-checker";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), checker({ typescript: true })],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        css: true,
+        setupFiles: "./src/tests/setup.ts",
+    },
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true, // needed for the Docker Container port mapping to work
+        strictPort: true,
+        port: 5173, // you can replace this port with any port
+    },
+    base: '/banners/'
+});
